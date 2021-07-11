5fd6b185fc30d5329777282abff0db7a2f5fdf00
Abilat01 <Abilat01@yandex.ru>
2021-07-11 19:33:34 +0300

    View

---
+++ ImageBackground/ViewControlers/ArmyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ArmyViewController.swift
+//  ImageBackground
+//
+//  Created by Danya on 11.07.2021.
+//
+
+import UIKit
+
+class ArmyViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
